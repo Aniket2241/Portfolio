0cb31e4cb1e994fab5861d792c77d0481c82c531
Aniket2241 <sprateek507@gmail.com>
2025-03-21 19:25:40 +0530

    fixed badge icons

--- src/Component/Badges.jsx
+++ src/Component/Badges.jsx
@@ -8,8 +8,8 @@ export default function Badges() {
         <h1 className='text-gray-300 text-4xl font-serif font-bold ml-23'>Badges</h1>
         <RiVerifiedBadgeFill className='text-blue-400 text-3xl'/></div>
       <div className='flex ml-23 mt-5 gap-10 '>
- <a href="https://www.credly.com/badges/bb640f6f-dd05-47ff-b672-7d388dc9957d/public_url " target='_blank'><img src="/badge1.png" alt="Cloud Security Foundations" title='See Credential' className='cursor-pointer hover:transform hover:scale-105   rounded-lg border-2 border-blue-400 bg-gray-700   w-[10vw]' /></a>
- <a href="https://www.credly.com/badges/04e1c174-533f-4d98-9474-558fac763e5f/public_url " target='_blank'><img src="/badge2.png" alt="Cloud Foundations"  title='See Credential' className=' cursor-pointer  hover:transform hover:scale-105   border-blue-400 border-2 rounded-2xl bg-gray-700 w-[10vw]' /></a></div>
+ <a href="https://www.credly.com/badges/bb640f6f-dd05-47ff-b672-7d388dc9957d/public_url " target='_blank'><img src="/badge1.png" alt="Cloud Security Foundations" title='See Credential' className='cursor-pointer hover:scale-105 rounded-lg border-2 border-blue-400 bg-gray-700 w-32 sm:w-40 md:w-48 lg:w-56'  /></a>
+ <a href="https://www.credly.com/badges/04e1c174-533f-4d98-9474-558fac763e5f/public_url " target='_blank'><img src="/badge2.png" alt="Cloud Foundations"  title='See Credential' className='cursor-pointer hover:scale-105 rounded-lg border-2 border-blue-400 bg-gray-700 w-32 sm:w-40 md:w-48 lg:w-56' /></a></div>
     </div>
   )
 }
